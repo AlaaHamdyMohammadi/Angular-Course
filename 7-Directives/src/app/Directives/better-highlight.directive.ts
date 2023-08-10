2e439cc4048128afb4113b13bdcfe21c227c0c00
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-08-10 20:27:19 +0200

    HostListener

--- 7-Directives/src/app/Directives/better-highlight.directive.ts
+++ 7-Directives/src/app/Directives/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -9,6 +9,16 @@ export class BetterHighlightDirective implements OnInit{
 
   ngOnInit(){
     this.render.setStyle(this.element.nativeElement, 'color', 'tomato');
+  }
+  @HostListener('mouseover') mouseOver(){
+    this.render.setStyle(this.element.nativeElement, 'backgroundColor', 'lightgreen');
+  }
+  @HostListener('mouseout') mouseOut(){
+    this.render.setStyle(
+      this.element.nativeElement,
+      'backgroundColor',
+      'lightblue'
+    );
   }
 
 }
