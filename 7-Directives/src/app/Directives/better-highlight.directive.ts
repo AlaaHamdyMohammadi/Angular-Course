8c58da21722c0eebf692c01d4d2c98fea0544a8b
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-08-10 20:40:52 +0200

    HostBinding

--- 7-Directives/src/app/Directives/better-highlight.directive.ts
+++ 7-Directives/src/app/Directives/better-highlight.directive.ts
@@ -1,24 +1,27 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'white';
+  @HostBinding('style.padding') padding: string = '0px';
 
   constructor(private element: ElementRef, private render: Renderer2) { }
 
   ngOnInit(){
-    this.render.setStyle(this.element.nativeElement, 'color', 'tomato');
+    this.render.setStyle(this.element.nativeElement, 'color', 'blue');
   }
   @HostListener('mouseover') mouseOver(){
-    this.render.setStyle(this.element.nativeElement, 'backgroundColor', 'lightgreen');
+    // this.render.setStyle(this.element.nativeElement, 'backgroundColor', 'lightgreen');
+    this.backgroundColor = 'lightcoral';
+    this.padding = '20px';
   }
   @HostListener('mouseout') mouseOut(){
-    this.render.setStyle(
-      this.element.nativeElement,
-      'backgroundColor',
-      'lightblue'
-    );
+    //this.render.setStyle(this.element.nativeElement,'backgroundColor','lightblue');
+    this.backgroundColor = 'lightgray';
+    this.padding = '10px';
+
   }
 
 }
