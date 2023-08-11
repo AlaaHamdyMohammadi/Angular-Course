81ba8b38e02ca4a6b8b421ae8170c123da4ed232
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-08-11 20:38:55 +0200

    Injecting

--- 9-Service/src/app/Components/account/account.component.ts
+++ 9-Service/src/app/Components/account/account.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { LoggingService } from './../../Services/logging.service';
 
 @Component({
   selector: 'app-account',
@@ -6,15 +7,19 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./account.component.css'],
 })
 export class AccountComponent {
-  @Input() account !: { name: string; status: string };
-  @Input() id !: number;
+  @Input() account!: { name: string; status: string };
+  @Input() id!: number;
   @Output() statusChanged = new EventEmitter<{
     id: number;
     newStatus: string;
   }>();
 
+  constructor(private Logging: LoggingService) {}
+
   onSetTo(status: string) {
     this.statusChanged.emit({ id: this.id, newStatus: status });
     console.log('A server status changed, new status: ' + status);
+    this.Logging.logStatusChange(status);
+
   }
 }
--- 9-Service/src/app/Components/new-account/new-account.component.ts
+++ 9-Service/src/app/Components/new-account/new-account.component.ts
@@ -1,18 +1,23 @@
+import { LoggingService } from './../../Services/logging.service';
 import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
 
+  constructor(private Logging: LoggingService){}
+  
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus,
     });
     console.log('A server status changed, new status: ' + accountStatus);
+    this.Logging.logStatusChange(accountStatus)
   }
 }
