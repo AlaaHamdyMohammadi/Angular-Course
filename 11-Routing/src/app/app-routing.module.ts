40e88b18e578a287708e4eec670debd4d82e3274
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-08-14 20:24:57 +0200

    update

--- 11-Routing/src/app/app-routing.module.ts
+++ 11-Routing/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { UsersComponent } from './Components/users/users.component';
+import { ServersComponent } from './Components/servers/servers.component';
+import { NotFoundPageComponent } from './Components/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, title: 'Home Page' },
+  { path: 'users', component: UsersComponent, title: 'User Page' },
+  { path: 'servers', component: ServersComponent, title: 'Server Page' },
+  { path: '**', component: NotFoundPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- 11-Routing/src/app/app.module.ts
+++ 11-Routing/src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ServersComponent } from './Components/servers/servers.component';
+import { EditServerComponent } from './Components/servers/edit-server/edit-server.component';
+import { ServerComponent } from './Components/servers/server/server.component';
+import { UsersComponent } from './Components/users/users.component';
+import { UserComponent } from './Components/users/user/user.component';
+import { FormsModule } from '@angular/forms';
+import { NotFoundPageComponent } from './Components/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServersComponent,
+    EditServerComponent,
+    ServerComponent,
+    UsersComponent,
+    UserComponent,
+    NotFoundPageComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
