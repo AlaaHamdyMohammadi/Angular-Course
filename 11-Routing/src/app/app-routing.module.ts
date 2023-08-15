04b2b92f86e32b3b6c91fdf2b04659511d9f47d5
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-08-15 20:30:42 +0200

    Update

--- 11-Routing/src/app/app-routing.module.ts
+++ 11-Routing/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './Components/home/home.component';
 import { UsersComponent } from './Components/users/users.component';
 import { ServersComponent } from './Components/servers/servers.component';
 import { NotFoundPageComponent } from './Components/not-found-page/not-found-page.component';
+import { UserComponent } from './Components/users/user/user.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent, title: 'Home Page' },
   { path: 'users', component: UsersComponent, title: 'User Page' },
+  { path: 'users/:id', component: UserComponent, title: 'User Page' },
   { path: 'servers', component: ServersComponent, title: 'Server Page' },
   { path: '**', component: NotFoundPageComponent },
 ];
